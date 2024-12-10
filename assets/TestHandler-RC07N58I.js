1817423bda903d503956adb7c7b1fe76ca14eff9
BoiseCodeCamp <joverall22@gmail.com>
2024-12-10 18:36:28 +0000

    Update from https://github.com/BoiseCodeCamp/www/commit/9342d5357c84a969fa963afed81dca9cae95f072

--- assets/TestHandler-RC07N58I.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index-Gg1RYP4f.js";class t extends s{constructor(){super(),this.on("IS_TESTED",this.onTest)}onTest(e){console.log("Recieved IS_TESTED",e)}}const r=new t;export{r as testHandler};

3702ead761ab1ba8b2bb93c7cc264125e72f1a38
BoiseCodeCamp <joverall22@gmail.com>
2026-03-26 14:23:24 +0000

    Update from https://github.com/BoiseCodeCamp/www/commit/3bf37fde82062b9dc6d012ddd9b08454fc239bcd

---
+++ assets/SessionizeEmbed-U0DlR0IE.js
@@ -0,0 +1,20 @@
+import{_ as c,m as a,q as d,s as l,c as m,o as u,a as h,x as p}from"./index-kpVSkofV.js";const f={props:{src:String,title:{type:String,default:"Sessionize"}},setup(r){const t=a(null),s=a("200px");function e(o){var i;t.value&&o.source===t.value.contentWindow&&((i=o.data)==null?void 0:i.type)==="sz-resize"&&(s.value=o.data.height+"px")}d(()=>window.addEventListener("message",e)),l(()=>window.removeEventListener("message",e));const n=m(()=>`<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <style>body { margin: 0; }</style>
+</head>
+<body>
+  <script type="text/javascript" src="${r.src}"><\/script>
+  <script>
+    var observer = new MutationObserver(function() {
+      parent.postMessage({ type: 'sz-resize', height: document.documentElement.scrollHeight }, '*');
+    });
+    observer.observe(document.body, { childList: true, subtree: true, attributes: true });
+    window.addEventListener('load', function() {
+      parent.postMessage({ type: 'sz-resize', height: document.documentElement.scrollHeight }, '*');
+    });
+  <\/script>
+</body>
+</html>`);return{iframe:t,height:s,srcdoc:n}}},g=["srcdoc","title"];function v(r,t,s,e,n,o){return u(),h("iframe",{ref:"iframe",srcdoc:e.srcdoc,frameborder:"0",style:p({width:"100%",height:e.height,border:"none",overflow:"hidden"}),title:s.title,scrolling:"no"},null,12,g)}const w=c(f,[["render",v]]);export{w as default};
